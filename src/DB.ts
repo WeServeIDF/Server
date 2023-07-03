7601a83f1a187ea9a00768ecc09f23b1924b0020
Azriel Lebowitz <azriellebowitz@gmail.com>
2023-07-03 21:38:39 +0300

    feat(DB): DB connected

---
+++ src/DB.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize';
+// const oracledb = require('oracledb');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+    dialect: 'oracle',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dialectOptions: {connectString: process.env.DB_CONNECTION_STRING}
+});
+
+
+// sequelize.sync().then(() => 
+//     console.info("db connection established"))
+//     .catch((err) =>
+//     console.error("db connection failed " + err.message));
+
+export default sequelize;
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response} from 'express';
 const app : Express= express();
 const port : number = 3000;
 const userController = require('./Controllers/userController');
+import sequelize from './DB';
 
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello World!');
@@ -9,6 +10,11 @@ app.get('/', (req : Request, res : Response) => {
 
 app.use('/user', userController);
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await sequelize.sync().then(() => 
+  console.info("db connection established"))
+  .catch((err) =>
+  console.error("db connection failed " + err.message));
+  
   console.log(`Express is listening at http://localhost:${port}`);
 });
